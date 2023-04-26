8f05298e2ccbbfb91e5d1842aa4825e760ecc4d1
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2023-04-26 07:08:23 +0600

    adding web dto. closes #5

---
+++ src/main/java/com/learning/beerorderservice/web/model/BeerOrderDto.java
@@ -0,0 +1,32 @@
+package com.learning.beerorderservice.web.model;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class BeerOrderDto extends BaseItem{
+
+    @Builder
+    public BeerOrderDto(UUID id, Integer version, OffsetDateTime createdDate, OffsetDateTime lastModifiedDate, UUID customerId, String customerRef, List<BeerOrderLineDto> beerOrderLines, OrderStatusEnum orderStatus, String orderStatusCallBackUrl) {
+        super(id, version, createdDate, lastModifiedDate);
+        this.customerId = customerId;
+        this.customerRef = customerRef;
+        this.beerOrderLines = beerOrderLines;
+        this.orderStatus = orderStatus;
+        this.orderStatusCallBackUrl = orderStatusCallBackUrl;
+    }
+
+    private UUID customerId;
+    private String customerRef;
+    private List<BeerOrderLineDto> beerOrderLines;
+    private OrderStatusEnum orderStatus;
+    private String orderStatusCallBackUrl;
+}
---
+++ src/main/java/com/learning/beerorderservice/web/model/OrderStatusEnum.java
@@ -0,0 +1,5 @@
+package com.learning.beerorderservice.web.model;
+
+public enum OrderStatusEnum {
+    NEW, READY, PICKED_UP
+}
