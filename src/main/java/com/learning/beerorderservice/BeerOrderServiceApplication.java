7100468fa9fcaafbb3671b31c91fc36880c0fe1d
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2023-04-26 10:28:02 +0600

    Update initial commit. closes #1

--- src/main/java/com/learning/beerorderservice/BeerOrderServiceApplication.java
+++ src/main/java/com/learning/beerorderservice/BeerOrderServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MsscBeerOrderServiceApplication {
+public class BeerOrderServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MsscBeerOrderServiceApplication.class, args);
+        SpringApplication.run(BeerOrderServiceApplication.class, args);
     }
 
 }
